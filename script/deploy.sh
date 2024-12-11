4a26ec6dedb22e0d456d45df3ac15506a1217a73
gyeongmin2121@gmail.com <gyeongmin2121@gmail.com>
2024-12-11 22:22:17 +0900

    health check 추가, deploy.sh 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,15 +1,56 @@
-  echo "Removing port {PORT} from load balancer"
-          sed -i '/localhost:{PORT}/d' /etc/nginx/nginx.conf
-          sudo nginx -t
-          sudo systemctl reload nginx
+current_port = {PORT}
+ip="127.0.0.1"
 
 
-sudo fuser -k {PORT}/tcp
+echo -e "http://$ip:$current_port/management/health_check"
+RESPONSE=$(curl -s http://$ip:$current_port/management/health_check)
+IS_ACTIVE=$(echo ${RESPONSE} | grep 'UP' | wc -l)
+if [ $IS_ACTIVE -eq 1 ];
+then
+  echo -e "nginx 설정파일에서 $current_port를 제거합니다."
+  sudo sed -i "/127.0.0.1:$current_port/d" /etc/nginx/nginx.conf
 
-nohup /usr/local/java/java21/bin/java -Dserver.port={PORT} -jar ~/target/*SNAPSHOT.jar > log 2>&1 &
+  sudo nginx -t
+  echo "nginx를 reload합니다."
+  sudo nginx -s reload
+
+  fuser -s -k -TERM $current_port/tcp
+
+  echo -e "jar파일을 $port포트에 실행합니다."
+  nohup java -jar -Dserver.port=${current_port} ~/target/gitAction_practice-0.0.1-SNAPSHOT.jar > log 2>&1 &
+
+  for retry in {1..10}
+  do
+    RESPONSE=$(curl -s http://$ip:$current_port/management/health_check)
+    PORT_HEALTH=$(echo ${RESPONSE} | grep 'UP' | wc -l)
+    if [ $PORT_HEALTH -eq 1 ];
+    then
+      break
+    else
+      echo -e "$ip:$current_port가 켜져있지 않습니다. 10초 슬립하고 다시 헬스체크를 수행합니다."
+      sleep 10
+    fi
+  done
+
+  if [ $PORT_HEALTH -eq 1 ];
+  then
+    echo -e "$ip:$current_port에 정상적으로 spring boot가 실행 중입니다."
+  else
+    echo -e "$ip:$current_port에 정상적으로 spring boot가 실행 중이 아닙니다."
+    exit 0
+  fi
+
+  echo -e "nginx 설정파일에 $ip:$current_port을 추가합니다."
+  sudo sed -i "/upstream servers {/ a \    server 127.0.0.1:$current_port;" /etc/nginx/nginx.conf
+
+  sudo nginx -t
+  echo "nginx를 reload합니다."
+  sudo nginx -s reload
+else
+  echo -e "$current_port포트에 spring boot가 실행중이 아닙니다."
+  exit 0
+fi
 
 
- echo "Adding port {PORT} back to load balancer"
-          sed -i '/upstream loadbalancer {/a \    server localhost:{PORT};' /etc/nginx/nginx.conf
-          sudo nginx -t
-          sudo systemctl reload nginx
+
+
