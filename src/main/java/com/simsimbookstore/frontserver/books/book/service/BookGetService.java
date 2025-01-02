8c0e54d3c190a75f06bca8a7b792f05acdd2b2ee
limchaehwan <chlim20@naver.com>
2025-01-02 15:45:18 +0900

    feat:태그관련 도서조회,카테고리 관련 도서조회

--- src/main/java/com/simsimbookstore/frontserver/books/book/service/BookGetService.java
+++ src/main/java/com/simsimbookstore/frontserver/books/book/service/BookGetService.java
@@ -18,6 +18,26 @@ public class BookGetService {
 
     private final BookClient bookClient;
 
+
+    //특정 도서를 제외한 동일 카테고리 내 인기 도서 추천 기능
+    public List<BookListResponse> getRecommendBooks(Long bookId, List<Long> categoryIdList) {
+        return bookClient.getRecommendBooks(bookId, categoryIdList);
+    }
+
+    /**
+     * 카테고리랑 관련된 도서 조회
+     *
+     * @param categoryId
+     * @param userId
+     * @param page
+     * @param size
+     * @return
+     */
+    public PageResponse<BookListResponse> getBooksByCategory(Long categoryId, Long userId, int page, int size) {
+        return bookClient.getBooksByCategory(categoryId, userId, page, size);
+    }
+
+
     /**
      * 최근 출판된 6개 상품 조회
      *
@@ -40,15 +60,35 @@ public class BookGetService {
 
     /**
      * 관리자가 모든 도서조회 페이징처리해서
+     *
      * @param page
      * @param size
      * @return
      */
     public PageResponse<BookListResponse> getAllBooks(int page, int size) {
-        return bookClient.getAllBooks(page,size);
+        return bookClient.getAllBooks(page, size);
     }
 
-    public BookResponseDto getBookByIdForUpdate(Long bookId){
+    /**
+     * 도서 수정을 위한 도서 정보 불러오기
+     *
+     * @param bookId
+     * @return
+     */
+    public BookResponseDto getBookByIdForUpdate(Long bookId) {
         return bookClient.getBookByIdForUpdate(bookId);
+    }
+
+    /**
+     * 특정 태그와 관련된 도서 조회
+     *
+     * @param tagId
+     * @param userId
+     * @param page
+     * @param size
+     * @return
+     */
+    public PageResponse<BookListResponse> getBooksByTag(Long tagId, Long userId, int page, int size) {
+        return bookClient.getBooksByTag(tagId, userId, page, size);
     }
 }
