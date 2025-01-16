72dc91cb8104b0d03280a7f49a012dd9f85ff1a2
kodongwon <dw9706@naver.com>
2025-01-16 17:12:06 +0900

    admin페이지에서 수동으로 쿠폰 발급할때 RabbitMq로 Message를 통해 발급되도록 변경

--- src/main/java/com/simsimbookstore/frontserver/coupon/service/CouponAdminService.java
+++ src/main/java/com/simsimbookstore/frontserver/coupon/service/CouponAdminService.java
@@ -1,8 +1,12 @@
 package com.simsimbookstore.frontserver.coupon.service;
 
+import com.simsimbookstore.frontserver.config.RabbitMqConfig;
 import com.simsimbookstore.frontserver.coupon.client.CouponAdminClient;
 import com.simsimbookstore.frontserver.coupon.dto.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +18,7 @@ import java.util.Map;
 @Transactional(readOnly = true)
 public class CouponAdminService {
     private final CouponAdminClient couponAdminClient;
+    private final RabbitTemplate rabbitTemplate;
 
     public CouponPolicyResponseDto createCouponPolicy(CouponPolicyRequestDto requestDto) {
         return couponAdminClient.createCouponPolicy(requestDto);
@@ -30,9 +35,17 @@ public class CouponAdminService {
     public PageResponseDto<CouponTypeResponseDto> getAllCouponType(int page, int size) {
         return couponAdminClient.getAllCouponType(page, size);
     }
-
+    // RabbitMq 사용해서 쿠폰 발급
     public void issueCoupons(IssueCouponsRequestDto requestDto) {
-        couponAdminClient.issueCoupons(requestDto);
+//        couponAdminClient.issueCoupons(requestDto);
+        List<Long> userIds = requestDto.getUserIds();
+        for (Long userId : userIds) {
+            IssueCouponsRequestDto rabbitMqRequestDto = IssueCouponsRequestDto.builder()
+                    .userIds(List.of(userId))
+                    .couponTypeId(requestDto.getCouponTypeId())
+                    .build();
+            rabbitTemplate.convertAndSend(RabbitMqConfig.EXCHANGE_NAME, RabbitMqConfig.COUPON_ISSUE_QUEUE_ROUTING_KEY, rabbitMqRequestDto);
+        }
     }
 
     public PageResponseDto<CouponResponseDto> getCoupons(Long userId, int page, int size, String sortField) {
